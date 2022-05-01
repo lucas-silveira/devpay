bc774edfa33e7f91353c18703a4ac5f983f9af14
Lucas Silveira <lugano1995@gmail.com>
2022-05-01 18:20:27 -0300

    chore: install dotenv and create setupTests

---
+++ setupTests.ts
@@ -0,0 +1,11 @@
+import 'jest-ts-auto-mock';
+import 'reflect-metadata';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+dotenv.config({
+  path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`),
+});
+
+console.log = jest.fn();
