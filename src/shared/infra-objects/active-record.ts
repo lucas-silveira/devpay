101152894abf0c6fe64f6935a79a1039fa15d804
Lucas Silveira <lugano1995@gmail.com>
2022-05-01 21:40:29 -0300

    fix: remove shared ActiveRecord

--- src/shared/infra-objects/active-record.ts
+++
@@ -1,5 +0,0 @@
-import { BaseEntity } from 'typeorm';
-
-export abstract class ActiveRecord extends BaseEntity {
-  abstract deserialize(): any;
-}
--- src/shared/infra-objects/index.ts
+++ src/shared/infra-objects/index.ts
@@ -1,3 +1,2 @@
 export * from './domain.exception';
-export * from './active-record';
 export * as HttpServiceSetup from './http-service-setup';
