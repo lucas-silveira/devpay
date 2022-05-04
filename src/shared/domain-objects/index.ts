3b339b4ca45c433e468792cbd60678d6dfe8ea08
Lucas Silveira <lugano1995@gmail.com>
2022-05-04 18:56:12 -0300

    feat: create VirtualObject and DomainEvent shared objects

--- src/shared/domain-objects/index.ts
+++ src/shared/domain-objects/index.ts
@@ -1,4 +1,6 @@
 export * from './payment-method.enum';
 export * from './entity';
 export * from './aggregate-root';
+export * from './virtual-object';
 export * from './value-object';
+export * from './event';
