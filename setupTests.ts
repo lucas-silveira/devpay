27ab428adaacdde62310546f3c073359d168b6ac
Lucas Silveira <lugano1995@gmail.com>
2022-05-03 23:36:32 -0300

    chore: install date-fns

--- setupTests.ts
+++ setupTests.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 dotenv.config({
   path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`),
