a3590cd12dbcfcae110cb2d7b50217b2c092bf78
Lucas Silveira <lugano1995@gmail.com>
2022-05-08 20:13:50 -0300

    feat: add validation to CreatePaymentDto

--- src/application/dtos/request/create-payment.dto.ts
+++ src/application/dtos/request/create-payment.dto.ts
@@ -1,12 +1,37 @@
+import { Type } from 'class-transformer';
+import * as Validator from 'class-validator';
+
 export class CustomerDto {
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.MaxLength(36)
   public readonly name: string;
+
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
+  @Validator.MaxLength(36)
   public readonly document: string;
 }
 
 export class PaymentDto {
+  @Validator.IsNotEmpty()
+  @Validator.IsInt()
   public readonly rid: number;
+
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
   public readonly oid: string;
+
+  @Validator.IsNotEmpty()
+  @Validator.IsNumber()
   public readonly amount: number;
+
+  @Validator.IsNotEmpty()
+  @Validator.ValidateNested()
+  @Type(() => CustomerDto)
   public readonly customer: CustomerDto;
+
+  @Validator.IsNotEmpty()
+  @Validator.IsString()
   public readonly cardToken: string;
 }
