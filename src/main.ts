2889eda07508775d2a3d3ceeb78b8c8da1b77121
Lucas Silveira <lugano1995@gmail.com>
2022-05-08 20:17:12 -0300

    feat: config cors, helmet and ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
