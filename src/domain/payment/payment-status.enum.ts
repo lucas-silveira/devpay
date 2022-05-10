195ae721cd745b79be39075e4fe7be13afce8ba1
Lucas Silveira <lugano1995@gmail.com>
2022-05-10 20:47:46 -0300

    feat: add new PaymentStatus option

--- src/domain/payment/payment-status.enum.ts
+++ src/domain/payment/payment-status.enum.ts
@@ -1,6 +1,7 @@
 export enum PaymentStatus {
   Pending = 'pending',
   Authorized = 'authorized',
+  NotAuthorized = 'not_authorized',
   Fraud_analysis_sent = 'fraud_analysis_sent',
   Fraud_analysis_approved = 'fraud_analysis_approved',
   Fraud_analysis_denied = 'fraud_analysis_denied',
