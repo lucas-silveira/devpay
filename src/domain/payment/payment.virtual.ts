c839cdb417f3e15877ca4367b7f29883662327d9
Lucas Silveira <lugano1995@gmail.com>
2022-05-10 16:09:41 -0300

    feat: add providerLiable field to Payment virtual

--- src/domain/payment/payment.virtual.ts
+++ src/domain/payment/payment.virtual.ts
@@ -3,8 +3,9 @@ import { PaymentStatus } from './payment-status.enum';
 export type Payment = {
   id: string;
   recipientId: number;
-  orderId: string;
   policyId: string;
+  orderId: string;
+  providerLiable: string;
   status: PaymentStatus;
   amount: number;
   paidAmount: number;
