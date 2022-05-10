4e6bff0507807349accd714e420ba9e956c5775a
Lucas Silveira <lugano1995@gmail.com>
2022-05-09 22:20:22 -0300

    feat: add new Hash function

--- src/shared/utils/hash.ts
+++ src/shared/utils/hash.ts
@@ -1,3 +1,12 @@
+import { randomBytes } from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 
 export const generateUuidV4 = (): string => uuidv4();
+
+export const generateRandomString = (size = 24): Promise<string> =>
+  new Promise((res, rej) => {
+    randomBytes(size, (err: Error, buff: Buffer) => {
+      if (err) return rej(err);
+      return res(buff.toString('base64'));
+    });
+  });
