c6dbac8a36f8c208cb4e5b817037a34450b1cc46
Lucas Silveira <lugano1995@gmail.com>
2022-05-11 01:15:17 -0300

    feat: add equal method to ValueObject abstract class

--- src/shared/domain-objects/value-object.ts
+++ src/shared/domain-objects/value-object.ts
@@ -1,5 +1,13 @@
+import { isEqual } from 'lodash';
+
 export abstract class ValueObject {
   protected setReadOnlyProperty(propertyName: keyof this, value: any): void {
     this[propertyName] = value;
+  }
+
+  public isEqualTo(anObject: Record<string, any>): boolean {
+    if (this === anObject) return true;
+    if (!(anObject instanceof ValueObject)) return false;
+    return isEqual(this, anObject);
   }
 }
