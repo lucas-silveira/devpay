52107585cd286d7fdb54c9dad03e97f1419c1bef
Lucas Silveira <lugano1995@gmail.com>
2022-05-11 00:04:51 -0300

    fix: improve Partial type

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,12 @@
+type OmitMethods<T> = Omit<
+  T,
+  {
+    [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
+  }[keyof T]
+>;
+
 type Plain<T> = {
-  [K in keyof T]: T[K];
+  [K in keyof OmitMethods<T>]: T[K] extends Record<string, any>
+    ? Plain<T[K]>
+    : T[K];
 };
