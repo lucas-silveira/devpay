3e0c70271881f5ab2b4aed5158a7e256e9be55c8
Lucas Silveira <lugano1995@gmail.com>
2022-05-14 00:25:36 -0300

    feat: use RecipientFactory at AppRecipientsSignUp

--- src/application/services/app-recipients-sign-up.service.ts
+++ src/application/services/app-recipients-sign-up.service.ts
@@ -1,11 +1,21 @@
 import * as Nest from '@nestjs/common';
+import { HttpException, InternalServerErrorException } from '@nestjs/common';
 import { Request } from '@application/dtos';
+import { RecipientFactory } from './recipient.factory';
 
 @Nest.Injectable()
 export class AppRecipientsSignUp {
   public async createRecipient(
     recipientDto: Request.CreateRecipientDto,
   ): Promise<any> {
-    return;
+    try {
+      const recipient = await RecipientFactory.from(recipientDto);
+    } catch (err) {
+      if (err instanceof HttpException) throw err;
+
+      throw new InternalServerErrorException(
+        'Error while executing AppRecipientsSignUp.createRecipient',
+      );
+    }
   }
 }
