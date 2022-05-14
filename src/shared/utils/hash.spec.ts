fa5ba38dc2e6758c1d26f2a9d4f327f3f3dc0333
Lucas Silveira <lugano1995@gmail.com>
2022-05-14 01:15:56 -0300

    tests: create test for utils Hash

---
+++ src/shared/utils/hash.spec.ts
@@ -0,0 +1,21 @@
+import { generateRandomKey, generateUuidV4 } from './hash';
+
+describe('Hash', () => {
+  describe('generateUuidV4', () => {
+    it('Should be able to generate an UUIDv4', () => {
+      const uuid = generateUuidV4();
+      expect(uuid).toBeTruthy();
+      expect(typeof uuid).toBe('string');
+      expect(uuid.length).toBe(36);
+    });
+  });
+
+  describe('generateRandomKey', () => {
+    it('Should be able to generate a random key', async () => {
+      const randomKey = await generateRandomKey();
+      expect(randomKey).toBeTruthy();
+      expect(typeof randomKey).toBe('string');
+      expect(randomKey.length).toBe(16);
+    });
+  });
+});
