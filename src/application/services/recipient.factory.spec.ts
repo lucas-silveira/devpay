406c762a63755008d476ffcb9d8f5a31c65cc2f9
Lucas Silveira <lugano1995@gmail.com>
2022-05-17 02:11:10 -0300

    tests: improve Application.RecipientFactory test

--- src/application/services/recipient.factory.spec.ts
+++ src/application/services/recipient.factory.spec.ts
@@ -1,17 +1,14 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import * as Nest from '@nestjs/common';
 import { DomainException } from '@shared/infra-objects';
 import { Recipient } from '@domain/recipient';
 import * as Mocks from '@infra/mocks';
-import { Request } from '@application/dtos';
 import { RecipientFactory } from './recipient.factory';
 
 describe('RecipientFactory', () => {
-  const recipientDto: Request.CreateRecipientDto =
-    Mocks.RecipientPlainObjectBuilder()
+  it('Should be able to create a Recipient', async () => {
+    const recipientDto = Mocks.RecipientPlainObjectBuilder()
       .withoutFields('id', 'secretKey', 'policyId', 'createdAt')
       .build();
-
-  it('Should be able to create a Recipient', async () => {
     const recipient = await RecipientFactory.from(recipientDto);
     const expectedRecipient = {
       firstName: 'John',
@@ -38,14 +35,17 @@ describe('RecipientFactory', () => {
   });
 
   it('Should be able to throw a DomainException if constructor throw it', async () => {
-    await expect(
-      RecipientFactory.from({ ...recipientDto, type: 'X' as any }),
-    ).rejects.toThrowError(DomainException);
+    const recipientDto = Mocks.RecipientPlainObjectBuilder()
+      .withFields({ type: 'X' as any })
+      .build();
+    await expect(RecipientFactory.from(recipientDto)).rejects.toThrowError(
+      DomainException,
+    );
   });
 
   it('Should be able to throw an InternalServerErrorException if an unknown error occurs', async () => {
     await expect(RecipientFactory.from(undefined)).rejects.toThrowError(
-      InternalServerErrorException,
+      Nest.InternalServerErrorException,
     );
   });
 });
