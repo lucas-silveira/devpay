e89f02e0da65c1de04575db050e62b4b3475ed09
Lucas Silveira <lugano1995@gmail.com>
2022-05-17 00:17:40 -0300

    feat: add new function checkIfHasEmptyFields to util ObjectChecker

--- src/shared/utils/object-checker.spec.ts
+++ src/shared/utils/object-checker.spec.ts
@@ -1,4 +1,4 @@
-import { fieldExists } from './object-checker';
+import { fieldExists, checkIfHasEmptyFields } from './object-checker';
 
 describe('ObjectChecker', () => {
   describe('fieldExists', () => {
@@ -23,6 +23,84 @@ describe('ObjectChecker', () => {
 
     it('Should be able to get false if we pass an undefined object', () => {
       expect(fieldExists(undefined, 'field1')).toBe(false);
+    });
+  });
+
+  describe('checkIfHasEmptyFields function', () => {
+    let callbackMock: jest.Mock;
+
+    beforeEach(() => {
+      callbackMock = jest.fn(() => undefined);
+    });
+
+    it('Should be able to not execute callback if a not empty value is passed', () => {
+      const value = 1;
+      const fieldName = 'value';
+
+      expect(() =>
+        checkIfHasEmptyFields(value, fieldName, callbackMock),
+      ).not.toThrow();
+      expect(callbackMock).not.toBeCalled();
+    });
+
+    it('Should be able to not execute callback if a not empty object is passed', () => {
+      const value = { field: 1 };
+      const fieldName = 'value';
+
+      expect(() =>
+        checkIfHasEmptyFields(value, fieldName, callbackMock),
+      ).not.toThrow();
+      expect(callbackMock).not.toBeCalled();
+    });
+
+    it('Should be able to execute callback 1 time if an empty value is passed', () => {
+      const value = undefined;
+      const fieldName = 'value';
+
+      expect(() =>
+        checkIfHasEmptyFields(value, fieldName, callbackMock),
+      ).not.toThrow();
+      expect(callbackMock).toBeCalledTimes(1);
+      expect(callbackMock).toBeCalledWith(fieldName);
+    });
+
+    it('Should be able to execute callback 1 time if an object with 1 empty field is passed', () => {
+      const value = { field: undefined };
+      const fieldName = 'obj';
+
+      expect(() =>
+        checkIfHasEmptyFields(value, fieldName, callbackMock),
+      ).not.toThrow();
+      expect(callbackMock).toBeCalledTimes(1);
+      expect(callbackMock).toBeCalledWith(`${fieldName}.field`);
+    });
+
+    it('Should be able to execute callback 2 time if an object with 2 empty fields is passed', () => {
+      const value = { field1: undefined, field2: undefined, field3: 1 };
+      const fieldName = 'obj';
+
+      expect(() =>
+        checkIfHasEmptyFields(value, fieldName, callbackMock),
+      ).not.toThrow();
+      expect(callbackMock).toBeCalledTimes(2);
+      expect(callbackMock).nthCalledWith(1, `${fieldName}.field1`);
+      expect(callbackMock).nthCalledWith(2, `${fieldName}.field2`);
+    });
+
+    it('Should be able to execute callback 2 time if nested objects with empty fields is passed', () => {
+      const value = {
+        field1: { field1_1: undefined, field1_2: 1 },
+        field2: undefined,
+        field3: 1,
+      };
+      const fieldName = 'obj';
+
+      expect(() =>
+        checkIfHasEmptyFields(value, fieldName, callbackMock),
+      ).not.toThrow();
+      expect(callbackMock).toBeCalledTimes(2);
+      expect(callbackMock).nthCalledWith(1, `${fieldName}.field1.field1_1`);
+      expect(callbackMock).nthCalledWith(2, `${fieldName}.field2`);
     });
   });
 });
--- src/shared/utils/object-checker.ts
+++ src/shared/utils/object-checker.ts
@@ -2,3 +2,18 @@ export const fieldExists = (
   obj: Record<string, unknown>,
   field: string,
 ): boolean => Object.keys(obj || {}).includes(field);
+
+export const checkIfHasEmptyFields = (
+  value: unknown,
+  fieldName: string,
+  callback: (value: unknown) => void,
+) => {
+  const isEmpty = value === null || value === undefined || value === '';
+
+  if (isEmpty) callback(fieldName);
+
+  if (typeof value === 'object')
+    Object.entries(value).forEach(([_fieldName, _value]) =>
+      checkIfHasEmptyFields(_value, `${fieldName}.${_fieldName}`, callback),
+    );
+};
