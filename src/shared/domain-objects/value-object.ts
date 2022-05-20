70bf60c423a25a890e88da71a9f6e034a6f1d2ab
Lucas Silveira <lugano1995@gmail.com>
2022-05-20 01:20:31 -0300

    refactor: improve ValueObject signature

--- src/shared/domain-objects/value-object.ts
+++ src/shared/domain-objects/value-object.ts
@@ -1,7 +1,10 @@
 import { isEqual } from 'lodash';
 
 export abstract class ValueObject {
-  protected setReadOnlyProperty(propertyName: keyof this, value: any): void {
+  protected setReadOnlyProperty<K extends keyof this>(
+    propertyName: K,
+    value: this[K],
+  ): void {
     this[propertyName] = value;
   }
 
