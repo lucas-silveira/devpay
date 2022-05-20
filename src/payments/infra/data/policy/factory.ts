38d1e6d199ba8636135d43a242c8d5fc4a53b111
Lucas Silveira <lugano1995@gmail.com>
2022-05-20 02:09:25 -0300

    feat: create Infra PolicyFactory

---
+++ src/payments/infra/data/policy/factory.ts
@@ -0,0 +1,48 @@
+import * as Nest from '@nestjs/common';
+import { ErrorLog } from '@shared/apm';
+import * as NestAddons from '@shared/nest-addons';
+import { Policy, ProviderLiable, Requirements } from '@payments/domain';
+import { PolicyActiveRecord } from './policy.ar';
+
+export class PolicyFactory {
+  public static toDomainObject(policyAR: PolicyActiveRecord): Policy {
+    try {
+      return new Policy(
+        policyAR.id,
+        policyAR.fee,
+        this.remakeRequirementsFrom(policyAR),
+        this.remakeProviderLiablesFrom(policyAR),
+        policyAR.createdAt,
+      );
+    } catch (err) {
+      new NestAddons.AppLogger(PolicyFactory.name).error(
+        new ErrorLog(err, `Error while remaking Policy from active record`, {
+          policyAR,
+        }),
+      );
+
+      if (err instanceof Nest.HttpException) throw err;
+
+      throw new Nest.InternalServerErrorException(
+        'Error while remaking Policy from active record',
+      );
+    }
+  }
+
+  private static remakeRequirementsFrom(
+    policyAR: PolicyActiveRecord,
+  ): Requirements {
+    return new Requirements(
+      policyAR.requirements.minAccountMonths,
+      policyAR.requirements.recipientType,
+    );
+  }
+
+  private static remakeProviderLiablesFrom(
+    policyAR: PolicyActiveRecord,
+  ): ProviderLiable[] {
+    return policyAR.providerLiables.map(
+      (pl) => new ProviderLiable(pl.paymentProviderId, pl.paymentMethod),
+    );
+  }
+}
