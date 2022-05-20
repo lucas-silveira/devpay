d3b0e409b7f4e58bfe1b3591184fcc372cd692bd
Lucas Silveira <lugano1995@gmail.com>
2022-05-20 01:22:03 -0300

    refactor: add vo extension to DomainEvent object

--- src/shared/domain-objects/index.ts
+++ src/shared/domain-objects/index.ts
@@ -2,5 +2,5 @@ export * from './payment-method.enum';
 export * from './entity';
 export * from './aggregate-root';
 export * from './value-object';
-export * from './event';
+export * from './event.vo';
 export * as Validator from './validator';
