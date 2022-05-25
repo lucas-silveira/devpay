6080b0f600ecc864d4a171d674b4a60e9ce6a30f
Lucas Silveira <lugano1995@gmail.com>
2022-05-25 18:14:32 -0300

    chore: create PaymentProviders migration

---
+++ src/payments/infra/data/migrations/1653512774081-CreatePaymentProviders.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreatePaymentProviders1653512774081 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'payment_providers',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'type',
+            type: 'enum',
+            enum: ['bank', 'acquirer'],
+          },
+          {
+            name: 'acceptedPaymentMethods',
+            type: 'json',
+          },
+          {
+            name: 'authToken',
+            type: 'varchar',
+            length: '24',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('payment_providers');
+  }
+}
