dc6a51d537d2b6489ee16b184666f895150947ed
Lucas Silveira <lugano1995@gmail.com>
2022-05-26 20:15:09 -0300

    fix: IPoliciesRepository.findOneById signature

--- src/payments/domain/policy/repository.interface.ts
+++ src/payments/domain/policy/repository.interface.ts
@@ -2,5 +2,5 @@ import { Policy } from './policy.entity';
 
 export interface IPoliciesRepository {
   save(policy: Policy): Promise<void>;
-  findOneById(id: number): Promise<Policy>;
+  findOneById(id: string): Promise<Policy>;
 }
