8b2b1b0f53a44387cf56cb4524a0f4127f9d6ceb
Lucas Silveira <lugano1995@gmail.com>
2022-05-26 23:21:51 -0300

    refactor: Utils Hash mongoose import

--- src/shared/utils/hash.ts
+++ src/shared/utils/hash.ts
@@ -1,5 +1,5 @@
 import { randomBytes } from 'crypto';
-import mongoose from 'mongoose';
+import { Types as MongoTypes } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 
 export const generateUuidV4 = (): string => uuidv4();
@@ -13,4 +13,4 @@ export const generateRandomKey = (size = 8): Promise<string> =>
   });
 
 export const generateObjectId = (): string =>
-  new mongoose.Types.ObjectId().toString();
+  new MongoTypes.ObjectId().toString();
