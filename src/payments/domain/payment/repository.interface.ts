4f35fde87c9128270c5f746bf9e0e487d70725a5
Lucas Silveira <lugano1995@gmail.com>
2022-05-27 00:50:37 -0300

    feat: add a new method create to IPaymentsRepository

--- src/payments/domain/payment/repository.interface.ts
+++ src/payments/domain/payment/repository.interface.ts
@@ -1,6 +1,7 @@
 import { Payment } from './payment.virtual';
 
 export interface IPaymentsRepository {
+  create(payment: Payment): Promise<void>;
   findOneById(id: string): Promise<Payment>;
   findByRecipientId(recipientId: number): Promise<Payment[]>;
   findByOrderId(orderId: string): Promise<Payment[]>;
