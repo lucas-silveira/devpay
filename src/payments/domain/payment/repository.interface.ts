bd579140cf41165dfbc91625b4d9049edf77f212
Lucas Silveira <lugano1995@gmail.com>
2022-05-27 02:35:05 -0300

    feat: improve IPaymentsRepository signature

--- src/payments/domain/payment/repository.interface.ts
+++ src/payments/domain/payment/repository.interface.ts
@@ -3,6 +3,15 @@ import { Payment } from './payment.virtual';
 export interface IPaymentsRepository {
   create(payment: Payment): Promise<void>;
   findOneById(id: string): Promise<Payment>;
-  findByRecipientId(recipientId: number): Promise<Payment[]>;
-  findByOrderId(orderId: string): Promise<Payment[]>;
+  findByRecipientId(
+    recipientId: number,
+    skip?: number,
+    limit?: number,
+  ): Promise<Payment[]>;
+  findByOrderId(
+    recipientId: number,
+    orderId: string,
+    skip?: number,
+    limit?: number,
+  ): Promise<Payment[]>;
 }
