c32aa2212c980265c2825e3b8f624d90d9b4087f
Lucas Silveira <lugano1995@gmail.com>
2022-05-31 00:23:04 -0300

    feat: connect RabbitMQ queue payments to App

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as Nest from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestAddons } from '@shared';
 import { AppModule } from './app.module';
 
@@ -19,6 +20,18 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [config.get<string>('rabbitMq.host')],
+        queue: config.get<string>('rabbitMq.queues.payments'),
+        noAck: false,
+      },
+    },
+    { inheritAppConfig: true },
+  );
+  await app.startAllMicroservices();
   await app.listen(config.get('app.httpPort') || 3000);
 }
 bootstrap();
